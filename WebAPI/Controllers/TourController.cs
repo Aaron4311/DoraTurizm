bb4df53ba9eca75d5e84c8d858048af488237d5e
Harun Karagöz <harunkaragoz1@gmail.com>
2024-10-31 15:18:11 +0300

    feat: Add Controllers

---
+++ WebAPI/Controllers/TourController.cs
@@ -0,0 +1,84 @@
+﻿using Autofac.Core;
+using Business.Abstract;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TourController : ControllerBase
+    {
+        private readonly ITourService _tourService;
+        public TourController(ITourService tourService)
+        {
+            _tourService = tourService;
+        }
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _tourService.GetAllAsync();
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result.Data);
+        }
+
+
+        [HttpGet("Get/{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _tourService.GetAsync(id);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result.Data);
+        }
+
+
+        [HttpGet("GetByUrl/{blogUrl}")]
+        public async Task<IActionResult> Get(string blogUrl)
+        {
+            var result = await _tourService.GetTourByUrl(blogUrl);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result.Data);
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add(Tour tour)
+        {
+            var result = await _tourService.AddAsync(tour);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result.Message);
+        }
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(Tour tour)
+        {
+            var result = await _tourService.UpdateAsync(tour);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result.Message);
+        }
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _tourService.DeleteAsync(id);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result.Message);
+        }
+    }
+}
