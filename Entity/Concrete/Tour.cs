22727be44f19fcfba8724e9d46b9ee4a5cd1bf11
Harun Karagöz <harunkaragoz1@gmail.com>
2024-10-31 13:34:38 +0300

    feat: Add Dto's And An Entity Class

---
+++ Entity/Concrete/Tour.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.AspNetCore.Http;
+namespace Entity.Concrete
+{
+    public class Tour : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DepartureDate { get; set; }
+        public string DepartureAndDestinationCity { get; set; }
+        public string? IntermediatePassageDate { get; set; }
+        public string? IntermediatePassageCity { get; set; }
+        public string ReturningDate { get; set; }
+        public string ReturningDepartureAndDestinationCity { get; set; }
+        public string Hotel { get; set; }
+        public string Time { get; set; }
+        public string Price { get; set; }
+        public byte[][] Images { get; set; }
+    }
+}
