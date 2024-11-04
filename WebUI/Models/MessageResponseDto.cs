eba5ce01772fc0f122922591d9a897e759ef5a4c
Harun Karagöz <harunkaragoz1@gmail.com>
2024-11-04 17:39:33 +0300

    feat: Add Models

---
+++ WebUI/Models/MessageResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WebUI.Models
+{
+    public class MessageResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string MessageContent { get; set; }
+    }
+}
