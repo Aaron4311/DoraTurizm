f726ff94f2903a2d611c3718176d54e57b9ed0db
Harun Karagöz <harunkaragoz1@gmail.com>
2024-11-04 17:35:43 +0300

    feat: Add IMessageDal with Dependency Injection

--- Business/Concrete/MessageManager.cs
+++ Business/Concrete/MessageManager.cs
@@ -15,7 +15,10 @@ namespace Business.Concrete
     public class MessageManager : IMessageService
     {
         private readonly IMessageDal _messageDal;
-
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
         public async Task<IResult> AddAsync(Message message)
         {
             await _messageDal.AddAsync(message);
