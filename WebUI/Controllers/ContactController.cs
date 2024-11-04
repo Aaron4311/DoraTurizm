f288cbc047198a8f052adf46b20a4415c4353691
Harun Karagöz <harunkaragoz1@gmail.com>
2024-11-04 17:38:52 +0300

    feat: Add Sending Message To Admin

--- WebUI/Controllers/ContactController.cs
+++ WebUI/Controllers/ContactController.cs
@@ -1,13 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebUI.Models;
+using WebUI.Services.Abstract;
 
 namespace WebUI.Controllers
 {
     public class ContactController : Controller
     {
+
+        private IMessageService _messageService;
+        public ContactController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
         [HttpGet("iletisim")]
         public async Task<IActionResult> Index()
         {
             return View();
         }
+
+
+        [HttpPost("SendMessage")]
+        public async Task<IActionResult> SendMessage([FromBody] MessageResponseDto messageResponseDto)
+        {
+            if (messageResponseDto == null)
+            {
+                return BadRequest("Bir Hata Oluştu. Lütfen Daha Sonra Tekrar Deneyiniz.");
+            }
+
+            await _messageService.AddAsync(messageResponseDto);
+            return Ok(); 
+        }
+
+
     }
 }
