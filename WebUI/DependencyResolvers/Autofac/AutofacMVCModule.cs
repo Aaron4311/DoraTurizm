43e7589bbf2647fa2a8933b628e1cfb4ea949e57
Harun Karagöz <harunkaragoz1@gmail.com>
2024-11-04 17:39:16 +0300

    feat: Add Dependency Resolver for MVC

---
+++ WebUI/DependencyResolvers/Autofac/AutofacMVCModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using System.Reflection;
+using WebUI.Services.Abstract;
+using WebUI.Services.Concrete;
+using Module = Autofac.Module;
+namespace WebUI.DependencyResolvers.Autofac
+{
+    public class AutofacMVCModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AuthManager>().As<IAuthService>().SingleInstance();
+
+            builder.RegisterType<MessageManager>().As<IMessageService>().SingleInstance();
+
+        }
+    }
+}
