5ecf39337f5a2d200a8d82f0ecb308ea66502650
Harun Karagöz <harunkaragoz1@gmail.com>
2024-11-08 14:32:05 +0300

    feat: Add TourResponseDto dto for api response

---
+++ WebUI/Models/TourResponseDto.cs
@@ -0,0 +1,22 @@
+﻿namespace WebUI.Models
+{
+    public class TourResponseDto
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string TourUrl { get; set; } 
+        public string DepartureDate { get; set; } 
+        public string DepartureCity { get; set; } 
+        public string DestinationCity { get; set; }
+        public string? IntermediatePassageDate { get; set; } 
+        public string? IntermediatePassageFirstCity { get; set; } 
+        public string? IntermediatePassageSecondCity { get; set; } 
+        public string ReturningDate { get; set; } 
+        public string ReturningDepartureCity { get; set; } 
+        public string ReturningDestinationCity { get; set; }
+        public string FirstHotel { get; set; } 
+        public string SecondHotel { get; set; } 
+        public decimal PriceForTwoRoom { get; set; } 
+        public decimal PriceForThreeRoom { get; set; } 
+    }
+}
